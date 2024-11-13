7ac38def9428730b9c3b8acff52541d68c5ecb2e
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2024-11-13 17:48:54 +0300

    setting: kafka is configuredp

---
+++ src/test/java/ru/gubern/kafkaapp/KafkaappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gubern.kafkaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
